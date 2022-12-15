8849c48ef50df34a541bb6b2031a96158e482c8b
[Alkandari-Y] <[yayalkandari@gmail.com]>
2022-12-15 06:45:51 +0300

    settings for cdn in installed apps

--- foodiez/settings.py
+++ foodiez/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
+    "upload",
+    "storages",
     "accounts.apps.AccountsConfig",
     "recipes.apps.RecipesConfig",
 ]
